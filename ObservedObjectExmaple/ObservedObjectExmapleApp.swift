6b056aac66bce0f753291ca7873da5f96694b787
Nour <mohamednour25@gmail.com>
2024-09-19 07:54:25 +0300

    Initial Commit

---
+++ ObservedObjectExmaple/ObservedObjectExmapleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObservedObjectExmapleApp.swift
+//  ObservedObjectExmaple
+//
+//  Created by administrator on 19/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct ObservedObjectExmapleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
